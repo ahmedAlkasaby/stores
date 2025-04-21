118f3702db19b0a08379a9c78d0b36b3c5e8533c
ahmed alkasaby <168755711+ahmedAlkasaby@users.noreply.github.com>
2025-04-21 16:51:35 +0200

    create store cart items

--- app/Http/Controllers/Api/CartItemController.php
+++ app/Http/Controllers/Api/CartItemController.php
@@ -3,9 +3,13 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Api\StoreCartItemsRequest;
+use App\Models\CartItem;
+use App\Models\Product;
+use App\Models\User;
 use Illuminate\Http\Request;
 
-class CartItemController extends Controller
+class CartItemController extends MainController
 {
 
     public function index()
@@ -14,9 +18,31 @@ class CartItemController extends Controller
     }
 
 
-    public function store(Request $request)
+    public function store(StoreCartItemsRequest $request)
     {
-        
+        // check qty is smaller than product stock
+        $product = Product::find($request->product_id);
+        if ($product->qty < $request->qty) {
+            return $this->messageError(__('validation.qty_greater_than_stock'));
+        }
+
+        // check if product is already in cart
+        $auth=Auth()->guard('api')->user();
+        $user=User::find($auth->id);
+        $cartItem=CartItem::where('product_id', $request->product_id)
+        ->where('user_id', $user->id)
+        ->first();
+        if($cartItem){
+           $cartItem->update([
+                'qty' => $request->qty,
+            ]);
+            return $this->messageSuccess(__('site.cart_item_updated'));
+        }
+
+        $user->cartItems()->create($request->validated());
+
+        return $this->messageSuccess(__('site.cart_item_added'));
+
     }
 
 
--- resources/lang/en/site.php
+++ resources/lang/en/site.php
@@ -4,4 +4,6 @@ return [
     'wishlist_removed' => 'Product removed from wishlist',
     'wishlist_added' => 'Product added to wishlist',
     'wishlists' => 'Wishlists',
+    'cart_item_updated' => 'Product updated in cart',
+    'cart_item_added' => 'Product added to cart',
 ];
