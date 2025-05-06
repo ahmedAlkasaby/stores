a01d83d9af38506d62287ad8aead60439cee0a76
ahmed alkasaby <168755711+ahmedAlkasaby@users.noreply.github.com>
2025-05-06 03:21:56 +0300

    create product index and show

---
+++ app/Http/Resources/Api/SizeResource.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Resources\Api;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class SizeResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->nameLang(),
+            'description' => $this->descriptionLang(),
+            'active' => $this->active,
+            'created_at' => $this->created_at->format('Y-m-d H:i:s'),
+        ];
+    }
+}
