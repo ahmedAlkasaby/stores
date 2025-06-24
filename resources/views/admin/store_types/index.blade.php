afbb9551dfcbd975cd0097081f66d21ca9013206
ahmed alkasaby <168755711+ahmedAlkasaby@users.noreply.github.com>
2025-06-25 00:47:34 +0300

    change blade header_of_table in layouts.tables

--- resources/views/admin/store_types/index.blade.php
+++ resources/views/admin/store_types/index.blade.php
@@ -6,7 +6,7 @@
 @include('admin.layouts.messages.success')
 @include('admin.layouts.messages.displayErrors')
 <div class="card">
-    @include('admin.layouts.header_of_table',['filter'=>false,'model'=>'store_types'])
+    @include('admin.layouts.tables.header_of_table',['filter'=>false,'model'=>'store_types'])
     @include('admin.store_types.includes.table')
 </div>
 
