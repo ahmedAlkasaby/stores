985900bb6c1e918c922c8078112b8ce5cb44c303
ahmed alkasaby <168755711+ahmedAlkasaby@users.noreply.github.com>
2025-06-27 08:05:01 +0300

    finish table users

--- resources/views/admin/roles/index.blade.php
+++ resources/views/admin/roles/index.blade.php
@@ -1,20 +1,21 @@
 @extends('admin.layouts.app')
 @section('title', __('site.stores'))
 
+@section('styles')
+
+@endsection
+
 @section('content')
 @include('admin.layouts.messages.success')
 @include('admin.layouts.messages.displayErrors')
 <div class="card">
     @include('admin.roles.includes.table')
 </div>
-</div>
 
-@section('mainFiles')
-<script src="{{asset("admin/assets/js/modal-add-new-address.js")}}"></script>
 @endsection
+
 
 @section('jsFiles')
 <script src="{{asset("admin/assets/vendor/libs/select2/select2.js")}}"></script>
 @include('admin.layouts.table.dataTableJs')
-@endsection
 @endsection
--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -2,9 +2,7 @@
 @section('title', __('site.users'))
 
 @section('styles')
-<link rel="stylesheet" href="https://cdn.datatables.net/1.13.6/css/jquery.dataTables.min.css" />
-<link rel="stylesheet" href="https://cdn.datatables.net/buttons/2.4.1/css/buttons.dataTables.min.css">
-
+@include('admin.layouts.table.datatablesCss')
 @endsection
 
 @section('content')
